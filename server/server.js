1d9cec56f82aa7b733d59de32aa4248ff84dc91a
= <=>
2023-04-12 23:49:25 +0530

    conversation changes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
-const io = require('socket.io')(5000,{cors:{origin:"*",methods:["GET","POST"],}});
-io.on('connection', socket =>{
-    const id = socket.handshake.query.id
-    socket.join(id);
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-    socket.on('send-message', ({recipients , text})=>{
-        recipients.forEach(recipient => {
-            const newRecipients = recipients.filter(r=> r !== recipient)
-            newRecipients.push(id)
-            socket.broadcast.to(recipient).emit('receive-message',{
-                recipients : newRecipients,sender:id,text
-            })
-        })
-    })
-})
+io.on("connection", (socket) => {
+  // ...
+  const id = socket.handshake.query.id;
+  socket.join(id);
+  console.log("connected", id);
+  socket.on("send-message", ({ recipients, text }) => {
+    console.log("send-message", recipients, text);
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit("receive-message", {
+        recipients: newRecipients,
+        sender: id,
+        text,
+      });
+    });
+    //       socket.broadcast.emit("receive-message", {
+    //     recipients: recipients,
+    //     sender: id,
+    //     text,
+    //   });
+  });
+});
+
+httpServer.listen(5000);
